df9b3090c631a4af12d5ade82c39c2831fe166e2
Ayoubslh <salhiayoub801@gmail.com>
2025-07-25 20:55:27 +0000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to the Store Web App</h1>
+        <p>This is a simple React application using Vite and Tailwind CSS.</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
