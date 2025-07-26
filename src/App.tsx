93ac7daf2f270d7ca204b226bf81ca42d7255c6e
Ayoubslh <salhiayoub801@gmail.com>
2025-07-26 13:26:57 +0000

    did somthing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Home from './ui/pages/Home';
 
 
 
@@ -6,12 +7,7 @@ import { useState } from 'react'
 function App() {
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to the Store Web App</h1>
-        <p>This is a simple React application using Vite and Tailwind CSS.</p>
-      </header>
-    </div>
+   <Home></Home>
   )
 }
 
---
+++ src/ui/pages/Home.tsx
@@ -0,0 +1,14 @@
+
+
+function Home() {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-menu">
+        <li className="navbar-item"><a href="/">Home</a></li>
+        <li className="navbar-item"><a href="/about">About</a></li>
+        <li className="navbar-item"><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
+export default Home;
