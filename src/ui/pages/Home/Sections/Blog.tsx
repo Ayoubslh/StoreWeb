e42d4abc4c03b6b7094dea3621971cd11c1e9a5a
Ayoubslh <salhiayoub801@gmail.com>
2025-07-28 20:44:52 +0000

    re

--- src/ui/pages/Home/Sections/Blog.tsx
+++ src/ui/pages/Home/Sections/Blog.tsx
@@ -58,7 +58,7 @@ export default function BlogSection() {
               <img
                 src={post.image}
                 alt={post.title}
-                className="w-full h-48 object-cover group-hover:scale-105 transition-transform duration-500"
+                className="w-full h-48 object-contain group-hover:scale-105 transition-transform duration-500"
               />
               <div className="absolute bottom-0 left-0 w-full bg-gradient-to-t from-black/80 to-transparent p-4">
                 <h4 className="text-white text-lg font-semibold group-hover:text-primary transition-colors duration-300">
